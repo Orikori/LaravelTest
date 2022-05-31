769e11cd4260cd387ad4d59baed26f9b4385cc66
d.artemev <dorvakd9@gmail.com>
2022-05-31 12:23:16 +0300

    Страница "На сайте ведутся технические работы"

---
+++ blog/resources/views/errors/503.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>На сайте ведутся технические работы</title>
+    <meta charset="utf-8" />
+
+    <style>
+        body{
+            font-family: Verdana, Arial, sans-serif;
+            font-size: 20px;
+            color : #000;
+        }
+        h1{
+            font-size: 30px;
+        }
+        p{
+            font-size: 22px;
+            padding: 0 0 0 10px;
+            margin:0;
+        }
+        .template{
+            padding: 0 0 10px 50px;
+            text-align: center;
+        }
+        .block{
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+        .bender{
+            max-width: 400px;
+        }
+    </style>
+</head>
+<body>
+<div class="block">
+    <div class="template">
+        <h1>На сайте ведутся технические работы</h1>
+        <p>На текущий момент наш интернет-ресурс недоступен. Мы просто решили сделать его немного лучше.</p>
+        <p>Обновите страницу через пару минут. Все будет работать.</p>
+    </div>
+</div>
+</div>
+</body>
+</html>
